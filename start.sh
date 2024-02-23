c38e9a56348eee99d8281a9de83645273ff523b6
Antoine T <antoine.t5656@gmail.com>
2024-02-23 02:14:34 +0100

    Update start.sh and stop.sh scripts, and docker-compose.yml

--- start.sh
+++ start.sh
@@ -3,23 +3,37 @@
 # Tentative de vérification si Docker est en cours d'exécution
 if ! docker info > /dev/null 2>&1; then
   echo "Docker n'est pas en cours d'exécution. Tentative de démarrage de Docker Desktop..."
-  # Remplacez par le chemin d'accès à Docker Desktop sur votre système si nécessaire
-  "/mnt/c/Program Files/Docker/Docker/Docker Desktop.exe" &
+  # Stocker le chemin d'exécution actuel
+  current_dir=$(pwd)
+  echo "Chemin d'exécution actuel : $current_dir"
+  
+  # déplacement vers le répertoire de Docker Desktop
+  echo "Changement de répertoire de travail à : $(pwd)"
+  cd "C:\Program Files\Docker\Docker"
+
+  # Démarrage de Docker Desktop
+  echo "Démarrage de Docker Desktop..."
+  "./Docker Desktop.exe"
+
+  # Revenir au dossier original
+  cd "$current_dir"
+  echo "Retour au chemin d'exécution actuel : $(pwd)"
+
   # Attendre un moment pour que Docker Desktop démarre
-  # Note : Ceci est une attente arbitraire ; ajustez selon les besoins de votre système
-  sleep 30
+  sleep 15
 else
   echo "Docker est déjà en cours d'exécution."
 fi
 
-# Naviguez au répertoire contenant votre fichier docker-compose.yml
-cd /chemin/vers/votre/projet
+#fermeture des conteneurs Docker
+docker-compose down
 
 # Lancez les services définis dans votre docker-compose.yml
 echo "Démarrage des conteneurs Docker..."
 docker-compose up -d
 
 echo "Les conteneurs Docker ont été démarrés."
-
 # Affichez un lien pour accéder au client web
 echo "Accédez au client web à l'adresse suivante : http://localhost:8080"
+
+sleep 15
--- stop.sh
+++ stop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Naviguez au répertoire contenant votre fichier docker-compose.yml
-cd /chemin/vers/votre/projet
-
 # Arrêtez et supprimez les conteneurs, les réseaux, les volumes et les images créés par `up`.
 echo "Arrêt des conteneurs Docker..."
 docker-compose down
